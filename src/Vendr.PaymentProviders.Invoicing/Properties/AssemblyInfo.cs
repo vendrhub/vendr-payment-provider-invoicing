32db60875177497172abddbf0ad2eac8feac180e
mattbrailsford <me@mattbrailsford.com>
2020-01-16 15:59:41 +0000

    Updated copyright assembly info

--- src/Vendr.PaymentProviders.Invoicing/Properties/AssemblyInfo.cs
+++ src/Vendr.PaymentProviders.Invoicing/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Vendr.PaymentProviders.Invoicing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Outfield Digital Ltd")]
 [assembly: AssemblyProduct("Vendr.PaymentProviders.Invoicing")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Outfield Digital Ltd 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
